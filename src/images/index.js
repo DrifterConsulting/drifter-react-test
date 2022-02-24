cafcefc8cdf0f9af68a54681aec0af74f4a818f7
Rohit Patel <rohitp20092@gmail.com>
2022-02-24 15:58:38 +0530

    Implement component to show blocks in grid format

---
+++ src/images/index.js
@@ -0,0 +1,15 @@
+import Block1 from './block1.png';
+import Block2 from './block2.png';
+import Block3 from './block3.png';
+import Block4 from './block4.png';
+import Block5 from './block5.png';
+import Block6 from './block6.png';
+
+export {
+  Block1,
+  Block2,
+  Block3,
+  Block4,
+  Block5,
+  Block6
+}
